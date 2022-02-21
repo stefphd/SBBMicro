1ccdb256a0e9945949b2554338821113b6d14e0c
stefphd <stefano.lovato.1@phd.unipd.it>
2022-02-21 20:44:03 +0000

    Update config.h

--- include/config.h
+++ include/config.h
@@ -54,18 +54,22 @@
 //DIGITAL OUT
 #define MTR_EN_PIN		16 //digital out for motor enable
 #define MTR_DIR_PIN		17 //digital out for motor direction
-#define RELAY_EN_PIN	33  //relay enable pin
+#define RELAY_EN_PIN	33 //digital out for relay enable pin
 
 //DIGITAL IN 
-
+#define ONOFF_STATE_PIN	34 //digital in for on/off buttun state (diode between onoff and this)	
 
 //.............................CONSTANTS & FACTORS.............................
 //TIMES
-#define SAMPLING_TIME	1e3 //sampling time of control (us) - must be consistent with that in Simulink! 
+#define SAMPLING_TIME		1e3 //sampling time of control (us) - must be consistent with that in Simulink! 
 // SAMPLING_TIME is also the fundamental sampling time of the system - all other sampling times are expresed in units of SAMPLING_TIME
 #define SPEED_SAMPLING_FAC	1 //sampling factor of speed sensor (expressed in units of SAMPLING_TIME) - now speed sensor data are read as soon as sent
 #define IMU_SAMPLING_FAC	1 //sampling factor of speed sensor (expressed in units of SAMPLING_TIME)
 #define MAG_SAMPLING_FAC	5 //sampling factor of speed sensor (expressed in units of SAMPLING_TIME)
+#define DEF_LED 			1000 //sampling factor of led in default mode
+#define LOG_LED 			500 //sampling factor of led in log mode
+#define MTP_LED 			250 //sampling factor of led in mtp mode
+#define ERR_LED 			10000 //sampling factor of led in error mode
 
 //OUTPUT DATA RATE
 #define ACC_ODR			3333 //ODR of accelerometer (sps)
@@ -99,7 +103,7 @@
 #define MAX_REFCUR		controlParams.maxCurrent //max reference current (according to ESCON studio and Simulink)
 #define MAX_REFTHROTTLE 1.0F //max throttle value (according to Simulink)
 #define MIN_VOLTAGE		18 //min battery voltage (undervoltage) (V)
-#define MAX_VOLTAGE		30 //max battery voltage (overvoltage) (V)
+#define MAX_VOLTAGE		35 //max battery voltage (overvoltage) (V)
 
 //....................................MACROS....................................
 
@@ -110,24 +114,24 @@
 #define CONVERT_ACTCURR_TO_A(X)			(GAIN_ACTCURR * X / (powf(2, ADC_RES) - 1) + OFFSET_ACTCURR) //macro for conversion of actual motor current to A
 #define CONVERT_CURRENT_TO_PWM(X)		(PWM_MIN * (powf(2, PWM_RES) - 1) + abs((PWM_MAX - PWM_MIN) * X / MAX_REFCUR * (powf(2, PWM_RES) - 1))) // convert the motor current to PWM value
 #define CONVERT_CURRENT_TO_DAC(X)		abs(X / MAX_REFCUR * (powf(2, DAC_RES) - 1)) //convert the motor current to DAC value
-#define CONVERT_TRHOTTLE_TO_DAC(X)  abs(X / MAX_REFTHROTTLE * (powf(2, DAC_RES) - 1)) //convert the throttle to DAC value
+#define CONVERT_TRHOTTLE_TO_DAC(X)  	abs(X / MAX_REFTHROTTLE * (powf(2, DAC_RES) - 1)) //convert the throttle to DAC value
 #define CONVERT_VOLTAGE_TO_V(X)			(GAIN_VOLTAGE * X / (powf(2, ADC_RES) - 1) + OFFSET_VOLTAGE) //macro for conversion of battery voltage to V
 #define CONVERT_FORKDISP_TO_MM(X)		(GAIN_FORKDISP * X / (powf(2, ADC_RES) - 1) + OFFSET_FORKDISP) //macro for conversion of front fork displacement to mm
 
 
 //ENABLES
-#define MTR_EN_STATE		HIGH //digital pin state for motor enable
-#define RELAY_EN_STATE		HIGH //digital pin state for relay enable
-#define MTR_ENABLE			digitalWriteFast(MTR_EN_PIN, MTR_EN_STATE), delay(100) //macro for motor enable
-#define RELAY_ENABLE		digitalWriteFast(RELAY_EN_PIN, RELAY_EN_STATE), delay(500) //macro for relay enable
-#define LEDON				digitalWriteFast(LED_BUILTIN, HIGH)
-#define LEDOFF				digitalWriteFast(LED_BUILTIN, LOW)
+#define MTR_EN_STATE	HIGH //digital pin state for motor enable
+#define RELAY_EN_STATE	HIGH //digital pin state for relay enable
+#define MTR_ENABLE		digitalWriteFast(MTR_EN_PIN, MTR_EN_STATE), delay(100) //macro for motor enable
+#define RELAY_ENABLE	digitalWriteFast(RELAY_EN_PIN, RELAY_EN_STATE), delay(500) //macro for relay enable
+#define LEDON			digitalWriteFast(LED_BUILTIN, HIGH)
+#define LEDOFF			digitalWriteFast(LED_BUILTIN, LOW)
 
 //POWER MANAGEMENT
-#define POWER_OFF			SNVS_LPCR |= (1<<6) //turn off MCU
+#define POWER_OFF		SNVS_LPCR |= (1<<6) //turn off MCU
 
 //...................................USER PARAMS.................................
-#define CPU_FREQ		    600e6 //CPU frequency (Hz)
+#define CPU_FREQ			600e6 //CPU frequency (Hz)
 #define MTR_CTRL_MODE		1 //motor control mode (0=PWM or 1=DAC)
 #define CW					LOW //digital pin state for clockwise torque
 #define NUM_ZERO_SAMPLES	500 //samples used to do zero of sensors
