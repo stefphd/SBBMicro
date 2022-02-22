0294433ddb511ee8678e2f19505c63d2063590eb
stefphd <stefano.lovato.1@phd.unipd.it>
2022-02-22 20:36:00 +0100

    Create include.h

---
+++ include/include.h
@@ -0,0 +1,24 @@
+//main include file
+//written by SL
+
+#ifndef __INCLUDE_H__
+#define __INCLUDE_H__
+
+/*
+include function prototypes
+*/
+#include <prototypes.h> 
+
+/*
+include implementations
+*/
+#include <Arduino.h>    //Arduino functions
+#include <libs.h>       //include libraries for buses, sensors, etc.
+#include <config.h>     //configuration file
+#include <objects.h>    //data types, variables, and objects
+#include <logger.h>     //include logger functions
+#include <sbbmtp.h>     //include mtp functions
+#include <functions.h>  //include user-defined functions
+#include <debug.h>      //include debug functions
+
+#endif
