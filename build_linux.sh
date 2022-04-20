60062278c2fcb761c3c4bfaf8c1026a4ae7ec27a
stefphd <stefano.lovato.1@phd.unipd.it>
2022-04-20 10:55:48 +0200

    Change directory names

--- build_linux.sh
+++ build_linux.sh
@@ -12,10 +12,10 @@ SRC="./src"
 BOARD="teensy:avr:teensy41"
 BOARD_OPTIONS="speed=600,usb=mtpserial,opt=o3std,keys=en-us"
 
-TEENSY_TOOLS="./build-tools/tools-linux"
+TEENSY_TOOLS="./tools/tools-linux"
 ARDUINO_FOLDER="/usr/share/arduino"
 
-HARDWARE="-hardware ./build-tools"
+HARDWARE="-hardware ./tools"
 TOOLS="-tools $TEENSY_TOOLS -tools $ARDUINO_FOLDER/tools-builder"
 BUILD_PATH=".build"
 CACHE_PATH=".cache"
