6eede3cdb15ecc0f5d8d180fa4f51939ac424566
stefphd <stefphd@users.noreply.github.com>
2023-03-02 07:01:14 +0000

    deploy: f1042d8bd29affc0a369488904805074cfd7870b

---
+++ index.js
@@ -0,0 +1,11 @@
+var index =
+[
+    [ "Files", "index.html#autotoc_md1", null ],
+    [ "Pre-requisites", "index.html#autotoc_md2", null ],
+    [ "Documentation", "index.html#autotoc_md3", null ],
+    [ "Code generation", "index.html#autotoc_md4", null ],
+    [ "Building", "index.html#autotoc_md5", null ],
+    [ "Make tools", "index.html#autotoc_md6", null ],
+    [ "MATLAB make", "index.html#autotoc_md7", null ],
+    [ "Known issues", "index.html#autotoc_md8", null ]
+];
--- navtreedata.js
+++ navtreedata.js
@@ -25,16 +25,7 @@
 var NAVTREE =
 [
   [ "SBB Micro", "index.html", [
-    [ "Introduction", "index.html", [
-      [ "Files", "index.html#autotoc_md1", null ],
-      [ "Pre-requisites", "index.html#autotoc_md2", null ],
-      [ "Documentation", "index.html#autotoc_md3", null ],
-      [ "Code generation", "index.html#autotoc_md4", null ],
-      [ "Building", "index.html#autotoc_md5", null ],
-      [ "Make tools", "index.html#autotoc_md6", null ],
-      [ "MATLAB make", "index.html#autotoc_md7", null ],
-      [ "Known issues", "index.html#autotoc_md8", null ]
-    ] ],
+    [ "Introduction", "index.html", "index" ],
     [ "Installation of the pre-requisites", "md_INSTALL_PREREQ.html", [
       [ "Windows", "md_INSTALL_PREREQ.html#autotoc_md10", [
         [ "Arduino builder", "md_INSTALL_PREREQ.html#autotoc_md11", null ],
